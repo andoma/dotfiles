8d05524f64fce28671230e24ecda2cf5be8a783d
Andreas Smas <andreas@lonelycoder.com>
2024-01-24 08:31:33 +0000

    Create ~/.profile if it doesn't exist

--- initsymlinks.sh
+++ initsymlinks.sh
@@ -3,3 +3,7 @@
 ln -sf ${PWD}/emacs ${HOME}/.emacs
 ln -sf ${PWD}/gitconfig ${HOME}/.gitconfig
 ln -sf ${PWD}/bash_aliases ${HOME}/.bash_aliases
+
+if [ ! -f ${HOME}/.profile ]; then
+    echo >${HOME}/.profile "source ~/.bash_aliases"
+fi
